447ab269df86572e75fdd18a400a2302c70f5d9a
LokiConF <alexandre.oliveira@aluno,imepac.edu.br>
2025-06-28 15:31:23 -0300

    28/05/25 15:29 Alek

--- clinica-medica-agendamento/src/main/java/br/edu/imepac/adminitrativo/exception/AuthenticationClinicaMedicaException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.imepac.adminitrativo.exception;
-
-public class AuthenticationClinicaMedicaException extends RuntimeException {
-
-    public AuthenticationClinicaMedicaException(String message) {
-        super(message);
-    }
-}
